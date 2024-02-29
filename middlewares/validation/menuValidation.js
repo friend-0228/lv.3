f42e3f1e1cf3a8a8da3ab1861242c431db28b6d9
friend-0228 <kwj2712@gamil.com>
2024-02-29 23:59:33 +0900

    [FEAT] 메뉴 조이 스키마 수정

--- middlewares/validation/menuValidation.js
+++ middlewares/validation/menuValidation.js
@@ -12,7 +12,9 @@ export const createMenuSchema = joi.object({
         'string.max': '제목은 최대 50글자를 초과할 수 없습니다.',
     }),
     image: joi.string().min(1).max(200).required().messages({}),
-    price: joi.number().min(0).required().messages({
-        'number.base': '메뉴 가격은 0보다 작을 수 없습니다.',
+    price: joi.number().strict().greater(0).required().messages({
+        'any.required': '메뉴 가격을 입력해주세요.',
+        'number.greater': '메뉴 가격은 0보다 작을 수 없습니다.',
+        'number.base': '숫자를 입력해주세요.', // 'number.base'를 사용하여 숫자 값이 예상되지만 잘못된 유형의 값이 전달된 경우에 대한 오류 메시지를 정의할 수 있다. ex: 문자열
     }),
 });
