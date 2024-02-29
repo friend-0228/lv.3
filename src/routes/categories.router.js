9270004008a2ba2817c0af33d3e3ce1c535af682
friend-0228 <kwj2712@gamil.com>
2024-02-29 20:08:57 +0900

    메뉴 등록,조회 까지 API 구현 완료

--- src/routes/categories.router.js
+++ src/routes/categories.router.js
@@ -4,9 +4,8 @@ import { prisma } from '../utils/prisma/index.js';
 const router = express.Router();
 
 // 카테고리 등록
-router.post("/categories", async (req, res, next) => {
+router.post('/categories', async (req, res, next) => {
     try {
-
         const { name } = req.body;
 
         // body를 입력받지 못한 경우 400 에러
@@ -18,30 +17,27 @@ router.post("/categories", async (req, res, next) => {
         const category = await prisma.categories.create({
             data: {
                 name,
-                order: (await prisma.categories.count()) + 1 // 현재 카테고리의 개수 + 1을 order 값으로 사용
+                order: (await prisma.categories.count()) + 1, // 현재 카테고리의 개수 + 1을 order 값으로 사용
             }, //  현재까지 데이터베이스에 저장된 카테고리의 개수를 반환
         });
 
-        return res.status(201).json({ message: "카테고리를 등록하였습니다" });
+        return res.status(201).json({ message: '카테고리를 등록하였습니다' });
     } catch (error) {
         console.error('카테고리 생성 중 오류:', error);
         return res.status(500).json({ error: '카테고리를 생성하는 중에 오류가 발생했습니다.' });
     }
 });
 
-
-  /** 카테고리 전체 목록 조회 API **/
-  router.get("/categories", async (req, res, next) => {
+/** 카테고리 전체 목록 조회 API **/
+router.get('/categories', async (req, res, next) => {
     try {
         // Prisma를 사용하여 모든 카테고리 조회
         const categories = await prisma.categories.findMany({
             // order를 기준으로 정렬
             orderBy: { order: 'asc' },
             // id, name, order 필드만 선택
-            select: { categoryId: true, name: true, order: true }
+            select: { categoryId: true, name: true, order: true },
         });
-
-
 
         // 조회된 카테고리 응답
         return res.status(200).json({ data: categories });
@@ -51,9 +47,20 @@ router.post("/categories", async (req, res, next) => {
     }
 });
 
+// 카테고리 삭제 API
+router.delete('/categories/:categoryId', async (req, res, next) => {
+    const { categoryId } = req.params;
+    if (!categoryId) {
+        return res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
+    }
 
+    const category = await prisma.categories.findFirst({ where: { categoryId: +categoryId } });
+    if (!category) {
+        return res.status(404).json({ message: '존재하지 않는 카테고리입니다.' });
+    }
 
-
-
+    await prisma.categories.delete({ where: { category: +categoryId } });
+    return res.status(200).json({ data: '카테고리 정보를 삭제하였습니다.' });
+});
 
 export default router;
--- src/routes/menus.router.js
+++ src/routes/menus.router.js
@@ -3,4 +3,64 @@ import { prisma } from '../utils/prisma/index.js';
 
 const router = express.Router();
 
+// 메뉴 등록 API
+router.post('/categories/:categoryId/menus', async (req, res, next) => {
+    const { categoryId } = req.params;
+    const { name, description, image, price } = req.body;
+
+    if (!categoryId || !name || !description || !image || !price) {
+        return res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
+    }
+
+    const category = await prisma.categories.findFirst({ where: { categoryId: +categoryId } });
+    if (!category) {
+        return res.status(404).json({ message: '존재하지 않는 카테고리입니다.' });
+    }
+
+    const menu = await prisma.menus.create({
+        data: {
+            name: name,
+            description: description,
+            image: image,
+            price: price,
+            order: (await prisma.categories.count()) + 1,
+            categoryId: +categoryId,
+        },
+    });
+
+    if (price <= 0) {
+        return res.status(400).json({ message: '메뉴 가격은 0보다 작을 수 없습니다.' });
+    }
+    return res.status(201).json({ message: '메뉴를 등록하였습니다.' });
+});
+
+// 카테고리별 메뉴 조회 API
+router.get('/categories/:categoryId/menus', async (req, res, next) => {
+    const { categoryId } = req.params;
+
+    const category = await prisma.categories.findFirst({ where: { categoryId: +categoryId } });
+    if (!categoryId) {
+        return res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
+    }
+
+    if (!category) {
+        return res.status(404).json({ message: '존재하지 않는 카테고리입니다.' });
+    }
+    const menus = await prisma.menus.findMany({
+        where: { categoryId: +categoryId },
+        select: {
+            menuId: true,
+            name: true,
+            image: true,
+            price: true,
+            order: true,
+            status: true,
+        },
+        orderBy: {
+            order: 'desc',
+        },
+    });
+    return res.status(200).json({ data: menus });
+});
+
 export default router;
